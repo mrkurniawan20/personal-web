880e02ae5b37c22c46a58fb64c6c57e68ed0511b
NewUser <mraflikurniawan20@gmail.com>
2025-02-19 00:17:41 +0700

    A LOT creating login page creating register page creating auth for login and register integrating session, fixing navbar

--- controllers/controller-v2.js
+++ controllers/controller-v2.js
@@ -4,27 +4,123 @@ const icon = {
 };
 // let blogs = []; kalo blogs:blogs ambil dari sini
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize'); //pake sequlize biar ga pake raw query kaya di controller v1
+const bcrypt = require('bcrypt'); //pake bcrypt buat enkripsi
 
-const config = require('../config/config.json');
-const { Blog } = require('../models');
+const config = require('../config/config.json'); //ambil config
+const { Blog, User } = require('../models'); //ambil Blog, sama User, ini table yang ada di sql
+
+const saltRounds = 10; //Untuk hashing berapa kali, sebenernya gausah dikasih vairable juga bisa, langusng angka di function
 // const { renderBlogEdit } = require('./controller-v1');
-const sequelize = new Sequelize(config.development);
+const sequelize = new Sequelize(config.development); //const sequelize buat masukin config ke function sequelize
+
+async function renderIndex(req, res) {
+  const user = await req.session.user;
+  res.render('index', {
+    currentPage: 'home',
+    user: user,
+    ...icon,
+  });
+}
+async function renderLogin(req, res) {
+  const user = await req.session.user;
+  res.render('auth-login', {
+    user: user,
+    currentPage: 'login',
+    ...icon,
+  });
+}
+async function renderRegister(req, res) {
+  res.render('auth-register', {
+    currentPage: 'register',
+    ...icon,
+  });
+}
+
+async function authLogin(req, res) {
+  const { email, password } = req.body;
+  // console.log(req.body);
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+  //check kalau usernya ada
+  if (!user) {
+    req.flash('error', 'User does not exist');
+    return res.redirect('/login');
+  }
+
+  //check kalau password salah
+  const isValidated = await bcrypt.compare(password, user.password);
+
+  if (!isValidated) {
+    req.flash('error', 'Password incorrect');
+    return res.redirect('/login');
+  }
+  let loggedInUser = user.toJSON();
+  console.log(loggedInUser);
+  //hapus passowrd di session
+  delete loggedInUser.password;
+  console.log(loggedInUser);
+
+  req.session.user = loggedInUser;
+  req.flash('success', `Login Succeed, Welcome ${loggedInUser.name}`);
+  res.redirect('/');
+}
+
+async function authRegister(req, res) {
+  const { name, email, password, confirmPassword } = req.body;
+  if (password != confirmPassword) {
+    req.flash('error', 'Password is not the same');
+    return res.redirect('/register');
+  }
+  const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+  if (user) {
+    req.flash('error', 'Email already exist');
+    return res.redirect('/register');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const newUser = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  const insertUser = await User.create(newUser);
+  req.flash('success', 'Register succeed!');
+  res.redirect('/login');
+}
+
+async function authLogout(req, res) {
+  //hapus user dari session
+  req.session.user = null;
+  res.redirect('/login');
+}
 
 async function renderBlog(req, res) {
+  const user = await req.session.user;
   const blogs = await Blog.findAll({
     order: [['createdAt', 'DESC']],
   });
   //   console.log(blogs);
   res.render('blog', {
+    user: user,
     currentPage: 'blog',
     blogs: blogs,
     ...icon,
   });
 }
 async function renderCreateBlog(req, res) {
+  const user = await req.session.user;
   //   console.log(blogs);
   res.render('blog-add', {
+    user: user,
     title: 'Add Blog',
     currentPage: 'addblog',
     blogs: Blog, //blog ambil dari models kalau ambil dari array(line 5) dia ambilnya "blogs" sama aja sebenernya
@@ -57,6 +153,7 @@ async function createBlog(req, res) {
 
 async function renderBlogDetail(req, res) {
   const id = req.params.id; //params yang /:id di blog
+  const user = await req.session.user;
   const chosenBlog = await Blog.findOne({
     where: {
       id: id,
@@ -69,6 +166,7 @@ async function renderBlogDetail(req, res) {
   // const chosenBlog = blogs[id]; //chosenBlog ngambil id dari blogs, misal index 0 = blog[0]
   res.render('blog-detail', {
     //UPDATE:  TIPE DATA TIDAK PERLU PAKE INDEX KAYA PAKE V1, KARENA TIPE DATANYA SUDAH OBJECT, BUKAN ARRAY
+    user: user,
     blog: chosenBlog, //nampilin blog
     title: 'Blog Details',
     currentPage: 'blog',
@@ -78,6 +176,7 @@ async function renderBlogDetail(req, res) {
 
 async function renderBlogEdit(req, res) {
   const id = req.params.id;
+  const user = await req.session.user;
   const chosenBlog = await Blog.findOne({
     where: {
       id: id,
@@ -89,6 +188,7 @@ async function renderBlogEdit(req, res) {
   // const chosenBlog = blogs[id]; //chosenBlog ngambil id dari blogs, misal index 0 = blog[0]
   res.render('blog-edit', {
     //UPDATE:  TIPE DATA TIDAK PERLU PAKE INDEX KAYA PAKE V1, KARENA TIPE DATANYA SUDAH OBJECT, BUKAN ARRAY
+    user: user,
     blog: chosenBlog, //nampilin blog
     title: 'Blog Details',
     currentPage: 'blog',
@@ -125,4 +225,50 @@ async function updateBlog(req, res) {
   res.redirect('/blog');
 }
 
-module.exports = { renderBlog, renderBlogDetail, createBlog, renderCreateBlog, deleteBlog, updateBlog, renderBlogEdit };
+async function renderProject(req, res) {
+  const user = await req.session.user;
+  res.render('myproject', {
+    user: user,
+    title: 'My Project',
+    currentPage: 'myproject',
+    ...icon,
+  });
+}
+
+async function renderTestimonial(req, res) {
+  const user = await req.session.user;
+  res.render('testimonial', {
+    user: user,
+    title: 'Testimonials',
+    currentPage: 'testimonial',
+    ...icon,
+  });
+}
+
+async function renderForm(req, res) {
+  const user = await req.session.user;
+  res.render('form', {
+    user: user,
+    title: 'Contact',
+    currentPage: 'form',
+    ...icon,
+  });
+}
+module.exports = {
+  renderIndex,
+  renderLogin,
+  renderRegister,
+  authLogin,
+  authRegister,
+  authLogout,
+  renderBlog,
+  renderBlogDetail,
+  renderBlogEdit,
+  renderCreateBlog,
+  renderProject,
+  renderTestimonial,
+  renderForm,
+  createBlog,
+  deleteBlog,
+  updateBlog,
+};
--- server.js
+++ server.js
@@ -2,12 +2,43 @@ const express = require('express'); //import express sebagai variable
 const hbs = require('hbs'); //import handlebars sebagai variable
 const app = express(); //assigned semua function yang ada di express sebagai variable
 const path = require('path'); //import modul path
+const flash = require('express-flash'); //import modul flash untuk register
+const session = require('express-session');
 const methodOverride = require('method-override'); //import modul methodOverride (download dulu, dependencies ada di package json)
 
-const { renderIndex, /*renderBlog,*/ /*renderBlogDetail*/ /*renderBlogEdit*/ /*createBlog*/ renderProject, renderTestimonial, /*renderCreateBlog,*/ renderForm, deleteBlog /*updateBlog*/ } = require('./controllers/controller-v1'); //import modul dari js controller
 const { formatDateToWIB, getRelativeTime } = require('./utils/time'); //import modul dari js time
+const {
+  /*renderIndex,*/
+  /*renderBlog,*/
+  /*renderBlogDetail*/
+  /*renderBlogEdit*/
+  /*createBlog*/
+  /*renderProject,*/
+  /*renderTestimonial,*/
+  /*renderCreateBlog,*/
+  /*renderForm,*/
+  /*deleteBlog*/
+  /*updateBlog*/
+} = require('./controllers/controller-v1'); //import modul dari js controller
 
-const { renderBlog, renderBlogDetail, createBlog, renderCreateBlog, renderBlogEdit, updateBlog } = require('./controllers/controller-v2');
+const {
+  renderIndex,
+  renderLogin,
+  renderRegister,
+  authLogin,
+  authRegister,
+  authLogout,
+  renderBlog,
+  renderBlogDetail,
+  renderBlogEdit,
+  renderCreateBlog,
+  renderProject,
+  renderTestimonial,
+  renderForm,
+  createBlog,
+  deleteBlog,
+  updateBlog,
+} = require('./controllers/controller-v2');
 const port = 3000; //port, angkanya assigned bebas, reccomended 4 digits diatas 3000
 
 const icon = {
@@ -16,10 +47,21 @@ const icon = {
 };
 
 app.set('view engine', 'hbs'); //setting view engine, pake hbs
+
+//modul yang dipake
 app.use(express.static('assets')); //assigned static kaya css,image,js macem assets, di folder 'assets'
 app.use(express.json()); //using function json
 app.use(express.urlencoded({ extended: true })); //using urlencoded function buat ambil data dari html method kaya post/get. extended:true buat bisa pake nested array object, crucial buat kalo mau pake method post/get
 app.use(methodOverride('_method')); //membolehkan app(express) pake method patch/delete, pokoknya selain post/get karena html hanya bisa mencerna method post/get
+app.use(flash());
+app.use(
+  session({
+    name: 'my-session',
+    secret: 'qwertyuiop',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.set('views', path.join(__dirname, './views')); //setting folder view engine
 hbs.registerPartials(__dirname + '/views/partials', function (err) {}); //__dirname buat ngasih tau folder yang mau dituju jadi, BASICALLY DIRNAME ITU FOLDER YANG DIBUKA ^^ YANG DI ATAS BROOOW
@@ -50,6 +92,15 @@ hbs.registerHelper('truncate', function (str, len) {
 
 //RENDER INDEX
 app.get('/', renderIndex);
+
+//RENDER LOGIN
+app.get('/login', renderLogin);
+app.get('/register', renderRegister);
+app.post('/auth-login', authLogin);
+app.post('/auth-register', authRegister);
+
+app.get('/logout', authLogout);
+
 //RENDER MYPROJECT
 app.get('/myproject', renderProject);
 //RENDER BLOG
